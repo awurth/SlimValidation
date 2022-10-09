43ca6801abd131f7e2b5240967ff844379db5469
Alexis Wurth <alexis.wurth@egerie.eu>
2022-10-09 12:56:32 +0200

    Add PHP-CS-Fixer

---
+++ .php-cs-fixer.dist.php
@@ -0,0 +1,38 @@
+<?php
+
+$finder = (new PhpCsFixer\Finder())
+    ->in(__DIR__)
+    ->exclude('var')
+    ->exclude('node_modules')
+    ->exclude('config')
+    ->exclude('public')
+;
+
+return (new PhpCsFixer\Config())
+    ->setRiskyAllowed(true)
+    ->setRules([
+        '@Symfony' => true,
+        '@Symfony:risky' => true,
+        'array_indentation' => true,
+        'cast_spaces' => [
+            'space' => 'none',
+        ],
+        'compact_nullable_typehint' => true,
+        'declare_strict_types' => true,
+        'no_superfluous_elseif' => true,
+        'no_useless_else' => true,
+        'no_useless_return' => true,
+        'ordered_imports' => [
+            'imports_order' => [
+                'class',
+                'function',
+                'const',
+            ],
+            'sort_algorithm' => 'alpha',
+        ],
+        'return_assignment' => true,
+        'strict_param' => true,
+        'void_return' => true,
+    ])
+    ->setFinder($finder)
+;
