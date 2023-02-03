da28af801ed61a9dd20b59bf8aaf84da07d6c953
Alexis Wurth <awurth.dev@gmail.com>
2023-02-04 00:38:05 +0100

    Add missing license headers

--- src/ValidationFactory.php
+++ src/ValidationFactory.php
@@ -2,6 +2,15 @@
 
 declare(strict_types=1);
 
+/*
+ * This file is part of the Awurth Validator package.
+ *
+ * (c) Alexis Wurth <awurth.dev@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 namespace Awurth\Validator;
 
 use Respect\Validation\Validatable;
